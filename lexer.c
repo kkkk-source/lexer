dd35c713569faab6579889ba93e78a8958e8172a
k <mol.tired@gmail.com>
2021-05-24 15:40:54 -0500

    get input from a type FILE

--- lexer.c
+++ lexer.c
@@ -1,88 +1,79 @@
+/*
+ * lexer.c - lexical analysis
+ *
+ */
+
+#include <string.h>
 #include <stdio.h>
+#include "lexer.h"
 
-enum tokkind {
-    END_OF_FILE,
-    IDENTIFIER,
-    DIVIDE,
-    MINUS,
-    MULTIPLY,
-    LEFT_PARENTHESIS,
-    PLUS,
-    RIGHT_PARENTHESIS,
-};
+#define SIZE 1000
 
-
-char *input_buf;
+char src_buf[SIZE];
 
 char *lextext;
 
+FILE *tokfd;
 
-int lex_next()
+int Lex_gettok(void)
 {
-    while (1) {
-	switch (*lextext) {
-	case '\n':
-	case '\t':
-	case '\r':
-	case ' ':
-	    ++lextext;
-	    break;
+  while (1) {
 
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-	    ++lextext;
-	    return IDENTIFIER;
-
-	case '/':
-	    ++lextext;
-	    return DIVIDE;
-
-	case '-':
-	    ++lextext;
-	    return MINUS;
-
-	case '*':
-	    ++lextext;
-	    return MULTIPLY;
-
-	case '(':
-	    ++lextext;
-	    return LEFT_PARENTHESIS;
-
-	case '+':
-	    ++lextext;
-	    return PLUS;
-
-	case ')':
-	    ++lextext;
-	    return RIGHT_PARENTHESIS;
-
-	case '\0':
-	    return END_OF_FILE;
-
-	default:
-	    fprintf(stderr, "Unexpected input found: %c\n", *lextext);
-	}
+    if (*lextext == '\0') {
+      if(fgets(src_buf, SIZE, tokfd) == NULL)
+        return END_OF_FILE;
+      lextext = src_buf;
     }
+
+    switch (*lextext) {
+      case '\n':
+      case '\t':
+      case '\r':
+      case ' ':
+        ++lextext;
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        ++lextext;
+        return IDENTIFIER;
+
+      case '/':
+        ++lextext;
+        return DIVIDE;
+
+      case '-':
+        ++lextext;
+        return MINUS;
+
+      case '*':
+        ++lextext;
+        return MULTIPLY;
+
+      case '(':
+        ++lextext;
+        return LEFT_PARENTHESIS;
+
+      case '+':
+        ++lextext;
+        return PLUS;
+
+      case ')':
+        ++lextext;
+        return RIGHT_PARENTHESIS;
+    }
+  }
 }
 
-
-int main(void)
-{
-    input_buf = "1 + 2   + 3   + 4 + 5";
-    lextext = input_buf;
-
-    for (int i = 0; i < 9; i++) {
-	printf("%d\n", lex_next());
-    }
-
-    return 0;
+void Lex_init(FILE *src) {
+  lextext = "";
+  tokfd = src;
 }
