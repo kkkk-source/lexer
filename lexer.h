bdc87346ce270574e1c06370738b9e7f75d4fd0b
k <mol.tired@gmail.com>
2021-05-24 15:40:15 -0500

    expose Lex_init and Lex_gettok and kind of tokens

---
+++ lexer.h
@@ -0,0 +1,11 @@
+#define END_OF_FILE 0
+#define IDENTIFIER  1
+#define DIVIDE 2
+#define MINUS 3
+#define MULTIPLY 4
+#define LEFT_PARENTHESIS 5
+#define PLUS 6
+#define RIGHT_PARENTHESIS 7
+
+void Lex_init(FILE *src);
+int Lex_gettok(void);
