ea6bde5f24c4981df8ff34d86b289d0d11163c68
k <mol.tired@gmail.com>
2021-05-24 17:39:46 -0500

    change from #define to enum

--- lexer.h
+++ lexer.h
@@ -1,11 +1,14 @@
-#define END_OF_FILE 0
-#define IDENTIFIER  1
-#define DIVIDE 2
-#define MINUS 3
-#define MULTIPLY 4
-#define LEFT_PARENTHESIS 5
-#define PLUS 6
-#define RIGHT_PARENTHESIS 7
+enum {
+  END_OF_FILE,
+  END_OF_INPUT,
+  IDENTIFIER,
+  DIVIDE,
+  MINUS,
+  MULTIPLY,
+  LEFT_PARENTHESIS,
+  PLUS,
+  RIGHT_PARENTHESIS,
+};
 
 void Lex_init(FILE * src);
 int Lex_gettok(void);
