cb940705967e953877e7d2795530325e7d43b590
k <mol.tired@gmail.com>
2021-05-24 23:11:59 -0500

    ignore unexpected characters

--- lexer.c
+++ lexer.c
@@ -8,33 +8,31 @@
 
 #define BUF_SIZE 2048
 
-// Buffer holds one line at a time of the source file srcfd
-char buf[BUF_SIZE];
-
-
-// Lexeme text character hold one character at a time of buf. For now, one
-// character represents a lexeme
+// lextext holds one character at a time of buf. For now, one character
+// represents a lexeme.
 char *lextext;
 
+// buf holds one line at a time of the source file srcfd.
+char buf[BUF_SIZE];
 
-// Source file descriptor. The file to get the source code from
+// srcfd is the source file descriptor, the file to get the source code from.
 FILE *srcfd;
 
-
-// Read the next line from the file descriptor srcfd
+// repeek puts the next line from the file descriptor srcfd into buf.
 void repeek(void)
 {
     if (fgets(buf, BUF_SIZE, srcfd) == NULL)
-	// There is no more input to read from
+	// There is no more lines to read from in srcfd.
 	*lextext = EOF;
     else
-	// Address lextext to the new line
+	// Address lextext to the first character of the new line.
 	lextext = buf;
 }
 
-// Src is the file to get the source code from. Src should be opened before
-// been pass through this initialization. Otherwise, the behavior is
-// unexpected.
+// Lex_init loads the first line of the src file into the buf and makes the
+// lextext to point to the buf's first character.  The source file src is the
+// file to get the source code from. Lex_init expects the file src to be
+// already opened.
 void Lex_init(FILE * src)
 {
     lextext = "";
@@ -42,12 +40,12 @@ void Lex_init(FILE * src)
     repeek();
 }
 
-// Return the enum that match the lexeme that lextex is currently holding
+// Lex_gettok returns the enum that match the lexeme that lextext is currently
+// pointing to.
 int Lex_gettok(void)
 {
     for (;;) {
 	switch (*lextext) {
-
 	case '\n':
 	case '\t':
 	case '\r':
@@ -56,7 +54,8 @@ int Lex_gettok(void)
 	    break;
 
 	case '\0':
-	    // When the current line has been consumed, peek the folliwng one
+	    // When the lextext points the '\0' character of the buf, put the
+	    // following line into buf.
 	    repeek();
 	    break;
 
@@ -98,8 +97,12 @@ int Lex_gettok(void)
 	    return RIGHT_PARENTHESIS;
 
 	case EOF:
-	    // The current file has been completly consumed
+	    // The current source file srcfd has been completly consumed.
 	    return END_OF_FILE;
+
+	default:
+	    // Unexpected character. Ignore it for now.
+	    lextext++;
 	}
     }
 }
