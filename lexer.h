114bfea68d907403f62a0354ec18a027c0b47f02
k <mol.tired@gmail.com>
2021-05-26 22:13:29 -0500

    docs(lexer): enum, lex_init(), and lex_gettok()

--- lexer.h
+++ lexer.h
@@ -1,3 +1,4 @@
+/* token types */
 enum {
     END_OF_FILE,
     END_OF_LINE,
@@ -11,5 +12,16 @@ enum {
     RIGHT_PARENTHESIS,
 };
 
+/*
+ * lex_init loads the first line of the src file into the buf and makes the
+ * lextext to point to the buf's first character.  The source file src is the
+ * file to get the source code from. Lex_init expects the file src to be
+ * already opened.
+ */
 void lex_init(FILE * src);
+
+/* 
+ * lex_gettok returns the token type (enum) that match the lexeme that lextext
+ * is currently pointing to. 
+ */
 int lex_gettok(void);
